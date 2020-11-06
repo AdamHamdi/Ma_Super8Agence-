9d75828bf95386c421e5a7f81400679ac748e78b
Adam Hamdi <prodev.webdesign@gmail.com>
2020-11-06 20:54:34 +0100

    maildev

--- src/Notification/ContactNotification.php
+++ src/Notification/ContactNotification.php
@@ -3,10 +3,29 @@
 namespace App\Notification;
 
 use App\Entity\Contact;
+use Twig\Environment;
 
 class ContactNotification{
+
+    protected $mailer;
+
+    protected $renderer;
+
+    public function __construct(\Swift_Mailer $mailer, Environment  $renderer){
+
+        $this->mailer = $mailer;
+        $this->renderer = $renderer;
+    }
+
     
     public function notify(Contact $contact){
-
+        $message =(new \Swift_Message('Agence :'.$contact->getProperty()->getTitle()))
+                    ->setFrom('noreply@sagence.fr')
+                    ->setTo('adamhamdi12@gmail.com')
+                    ->setReplyTo($contact->getEmail())
+                    ->setBody($this->renderer->render('emails/contact.html.twig',[
+                        'contact'=>$contact
+                    ]),'text/html');
+                    $this->mailer->send($message);
     }
 }
