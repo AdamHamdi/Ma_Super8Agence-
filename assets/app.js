f6c0ca23222bc91bc651d387bd9795177b4b9e72
Adam Hamdi <prodev.webdesign@gmail.com>
2020-11-07 01:49:42 +0100

    intaller encore

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,29 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+
 import './styles/app.css';
+
+// $ = 'jQuery';
+
+// require('select2')
+// $('select').select2()
+import $ from 'jquery';
+
+
+
+import 'select2';
+
+
+$('select').select2();
+
+
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+
+
+
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
